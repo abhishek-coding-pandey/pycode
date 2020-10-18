e5b4d0d93234b08696bfe1e577276ce80663043f
abhishek-coding-pandey <73053043+abhishek-coding-pandey@users.noreply.github.com>
2020-10-18 12:32:49 +0530

    Add files via upload

---
+++ say my name.py
@@ -0,0 +1,15 @@
+duct1 = ["12234", "1333456", "122347"]
+
+print("hello this is python")
+print("this programm is for testing that you are my school member or not if yes then ")
+print("you will get the percentage")
+print("enter your exam id")
+wwe = (input())
+greet=("you total marks are ")
+if duct1[0] == wwe:
+    print(greet,"900/1000")
+elif duct1[1]==wwe:
+    print( greet,"999/1000")
+elif duct1[2]==wwe:
+    print("you are fail","222/1000","re-exams to are going conducted on 19/12/2020 last chance ")
+else : print("id not found")
